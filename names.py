4d5c849cc2b267eb73902ae8e1afc7ac71ba563d
Jagannadh Bevara <jagannadh.bevara@rediffmail.com>
2016-07-19 22:30:00 +0530

    Commit 2

--- names.py
+++ names.py
@@ -1,5 +1,5 @@
 import mysql.connector
-conn = mysql.connector.connect(user='root', password='ver64pad', host='localhost', database='db1907')
+conn = mysql.connector.connect(user='root', password='ver64pad', host='localhost', database='entransys1')
 mycursor = conn.cursor()
 mycursor.execute("Show Tables")
 print mycursor.fetchall()
